f9f8d019480a4d1fa612277d6a693f17c4b3bab2
Valentin Siegel <valentin.siegel@smartcom.com>
2019-10-04 17:23:53 +0200

    Initial Commit

---
+++ XcodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeTest
+//
+//  Created by Valentin on 04/10/2019.
+//  Copyright © 2019 Valentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
